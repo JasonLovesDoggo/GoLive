b069e194c9b60c97373ffecbc2f9eed6f11788ea
unknown <nico.luu.to@gmail.com>
2024-06-07 22:50:45 -0400

    added some default frameworks

--- containerizer/commands.default.py
+++ containerizer/commands.default.py
@@ -14,15 +14,54 @@ class FRAMEWORKS(enum.Enum):
     DJANGO = "django"
     FLASK = "flask"
     FASTAPI = "fastapi"
+    RAILS = "rails"
+    EXPRESS = "express"
+    SPRING = "spring"
+    LARAVEL = "larvel"
 
 
 
 OPTIONS: Dict[FRAMEWORKS, Dict[COMMAND_TYPES, List[str]]] = {
     FRAMEWORKS.DJANGO: {
         COMMAND_TYPES.PRE_RUN: [
-            "python3 manage.py makemigrations --no-input",
-            "python3 manage.py migrate --no-input",
-            "python3 manage.py collectstatic --no-input",
+            "python3 {mainFile} makemigrations --no-input",
+            "python3 {mainFile} migrate --no-input",
+            "python3 {mainFile} collectstatic --no-input",
+        ]
+    },
+    FRAMEWORKS.FLASK: {
+        COMMAND_TYPES.PRE_RUN: [
+            "export FLASK_APP={mainFile}",
+            "export FLASK_ENV={env}",
+            "flask db upgrade",
+        ]
+    },
+    FRAMEWORKS.RAILS: {
+        COMMAND_TYPES.PRE_RUN: [
+            "bundle install",
+            "rails db:migrate",
+            "rails assets:precompile",
+        ]
+    },
+    FRAMEWORKS.EXPRESS: {
+        COMMAND_TYPES.PRE_RUN: [
+            "npm install",
+            "npm run build",
+        ]
+    },
+    FRAMEWORKS.LARAVEL: {
+        COMMAND_TYPES.PRE_RUN: [
+            "composer install",
+            "php artisan migrate",
+            "php artisan config:cache",
+            "php artisan route:cache",
+            "php artisan view:cache",
+        ]
+    },
+    FRAMEWORKS.SPRING: {
+        COMMAND_TYPES.PRE_RUN: [
+            "./mvnw clean install",
+            "./mvnw spring-boot:run",
         ]
     }
 }
