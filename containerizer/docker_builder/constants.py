2d21ceb47904ef3a679070c4e2ea537f402d389c
Jason Cameron <git@jasoncameron.dev>
2024-06-08 03:23:50 +0000

    Add basics of template engine
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- containerizer/docker_builder/constants.py
+++ containerizer/docker_builder/constants.py
@@ -1,9 +1,40 @@
 import enum
+from re import L
 from typing import Dict, List
 
 
 class LANGUAGES(enum.Enum):
     PYTHON = "python"
+    JAVASCRIPT = "javascript"
+    RUBY = "ruby"
+    GO = "go"
+    DART = "dart"
+    SWIFT = "swift"
+    CPP = "cpp"
+    C = "c"
+    CSHARP = "csharp"
+    KOTLIN = "kotlin"
+    SCALA = "scala"
+    RUST = "rust"
+    PHP = "php"
+    
+
+
+INSTALL_COMMANDS: Dict[LANGUAGES, str] = {
+    LANGUAGES.PYTHON: "pip install -r requirements.txt",
+    LANGUAGES.JAVASCRIPT: "npm install",
+    LANGUAGES.RUBY: "bundle install",
+    LANGUAGES.GO: "go mod download",
+    LANGUAGES.DART: "flutter pub get",
+    LANGUAGES.SWIFT: "swift package resolve",
+    LANGUAGES.CPP: "make",
+    LANGUAGES.C: "make",
+    LANGUAGES.CSHARP: "dotnet restore",
+    LANGUAGES.KOTLIN: "gradle build",
+    LANGUAGES.SCALA: "sbt compile",
+    LANGUAGES.RUST: "cargo build",
+    LANGUAGES.PHP: "composer install",
+}
 
 
 VERSIONS: Dict[LANGUAGES, List[str]] = {
@@ -16,5 +47,5 @@ VERSIONS: Dict[LANGUAGES, List[str]] = {
         "3" "3.12",
         "3.13.0b2",
     ]
-    
+
 }
--- containerizer/docker_builder/generator.py
+++ containerizer/docker_builder/generator.py
@@ -1,13 +1,45 @@
-from constants import LANGUAGES, VERSIONS
+from typing import Optional
+from constants import *
 from containerizer.defaults import FRAMEWORKS
-def dockerize(language: LANGUAGES, framework:FRAMEWORKS , version: str):
+from string import Template
+
+
+def dockerize(language: LANGUAGES, framework:FRAMEWORKS , version: str, install_command: Optional[str] = None):
     assert language in LANGUAGES, f"Language {language} not supported"
     assert version in VERSIONS[language], f"Version {version} not supported for {language}"
+    if install_command is None:
+        install_command = INSTALL_COMMANDS[language]
 
-    with open(f"./templates/{language}/{framework}") as raw_dockerfile:
+    with open(f"./templates/{language}/{framework}", "r") as raw_dockerfile:
         dockerfile = raw_dockerfile.read()
+        dockerfile = process(dockerfile, language, framework, version, install_command)
 
 
     # Dockerize the language and version
     pass
 
+
+def process(dockerfile: str, language: LANGUAGES, framework: FRAMEWORKS, version: str, install_command: str):
+    # get all variables in the dockerfile dynamically and replace them with the values
+    processed = Template(dockerfile).substitute(
+        VERSION=version,
+        INSTALL_COMMAND=install_command
+    )
+
+
+
+
+
+
+
+
+def test_dockerize():
+    command = dockerize(LANGUAGES.PYTHON, FRAMEWORKS.FLASK, "3.7")
+    DOCKERFILE = """
+    FROM python:3.7-alpine
+    WORKDIR /app
+    COPY . /app
+    RUN pip install -r requirements.txt
+    CMD ["python", "app.py"]
+    """
+    assert command == DOCKERFILE
