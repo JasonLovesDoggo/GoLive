6b52cf0d0cfd2067cd967cacb58759e50a10f4eb
Jason Cameron <git@jasoncameron.dev>
2024-06-08 22:50:02 +0000

    add hcl
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

---
+++ autodeploy/.terraform.lock.hcl
@@ -0,0 +1,19 @@
+# This file is maintained automatically by "tofu init".
+# Manual edits may be lost in future updates.
+
+provider "registry.opentofu.org/hashicorp/google" {
+  version = "5.32.0"
+  hashes = [
+    "h1:6L9Fn62dw0evxJ5BLsPFdcKsbbEQC0XU1FjHJk7E9oM=",
+    "zh:19809f3da85ab470539a2a10832cc1b53d4034f75d2003e0bccb52d0977236ff",
+    "zh:273a130c91cf3b592916b9532048fb366fe37567a6769029fb496bda8540c896",
+    "zh:3f00bdfa1af87df53d6625d1b65fd7ac7f0b6b6e8c210d9dedddb8b08573e1ea",
+    "zh:71acdb468e9a0ec881ea0f9cdaacc204363a0172251b5f3e8ea9a1f7dc5c224f",
+    "zh:76624b4f2a7e7f2733086ab03bbe5166ada4dcc4362beb769e01efd75804c83d",
+    "zh:8f5245561472ff6557905bb8ca991b4deba29a71b28c56d2e29d7f50d428d53f",
+    "zh:96d131d5577071ec7ab1a6453b53084b001e9aa1113b688ef5de3d3b0b624ea3",
+    "zh:98e9bea6418912940aa3767dcb32ffe4cbcbb08d566e52f031f09433f2d2fc47",
+    "zh:ab1fee78c43dc09432542dee28368edb7869e6d4e6688518c566bcec2e7df83a",
+    "zh:eefdd86d3ed1055264f4b144d5ba936192cb10b4abf13624e7bb8996de509353",
+  ]
+}
---
+++ autodeploy/main.tf
@@ -0,0 +1,55 @@
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-custom-mode-network"
+  project                 = ${PROJECT_ID}
+  auto_create_subnetworks = false
+  mtu                     = 1460
+}
+
+resource "google_compute_subnetwork" "default" {
+  name          = "my-custom-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-west1"
+  network       = google_compute_network.vpc_network.id
+}
+
+
+# Create a single Compute Engine instance
+resource "google_compute_instance" "default" {
+  name         = "flask-vm"
+  machine_type = "e2-micro"
+  zone         = "us-west1-a"
+  tags         = ["ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  # Install Docker
+ metadata_startup_script = <<-EOT
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo usermod -aG docker $USER
+  EOT
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.default.id
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
+}
