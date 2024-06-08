310764d6b759f28acfaa8f3572d20365eaa29c3f
unknown <nico.luu.to@gmail.com>
2024-06-08 00:44:44 -0400

    updated defaults.py

--- sample-project/app.py
+++ sample-project/app.py
@@ -1,12 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect, url_for, render_template
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'uploaded_files'
+ALLOWED_EXTENSIONS = {'zip', 'tar', 'gz'}
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Here you can add your deployment logic
+        return 'File successfully uploaded and deployed'
+    return 'File type not allowed'
+
+if __name__ == '__main__':
+    app.run(debug=True)
