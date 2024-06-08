a688979209aad3e1c5e951f322137a70a3eb2a3f
Jason Cameron <git@jasoncameron.dev>
2024-06-08 02:45:13 +0000

    app is trolling
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- sample-project/app2.py
+++ sample-project/app2.py
@@ -1,12 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
-
-
 @app.route("/")
-def home():s
+def home():
     return "WOWWW I'VE BEEN DEPLOYEED THANK THE GODS!"
-
 
 if __name__ == "__main__":
     app.run()
