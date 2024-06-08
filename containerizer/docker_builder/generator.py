d917ef1c584a22c41ee0401f3f01767c46905d7d
Jason Cameron <git@jasoncameron.dev>
2024-06-08 04:43:56 +0000

    add init to make module
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- containerizer/docker_builder/generator.py
+++ containerizer/docker_builder/generator.py
@@ -1,36 +1,51 @@
 from typing import Optional
 from constants import *
-from containerizer.defaults import FRAMEWORKS
+from containerizer.defaults import *
 from string import Template
 
 
-def dockerize(language: LANGUAGES, framework:FRAMEWORKS , version: str, install_command: Optional[str] = None):
-    assert language in LANGUAGES, f"Language {language} not supported"
-    assert version in VERSIONS[language], f"Version {version} not supported for {language}"
-    if install_command is None:
-        install_command = INSTALL_COMMANDS[language]
-
-    with open(f"./templates/{language}/{framework}", "r") as raw_dockerfile:
-        dockerfile = raw_dockerfile.read()
-        dockerfile = process(dockerfile, language, framework, version, install_command)
-
-
-    # Dockerize the language and version
-    pass
-
-
-def process(dockerfile: str, language: LANGUAGES, framework: FRAMEWORKS, version: str, install_command: str):
+def process(
+    dockerfile: str,
+    language: LANGUAGES,
+    framework: FRAMEWORKS,
+    version: str,
+    install_command: str,
+    run_command: str,
+):
     # get all variables in the dockerfile dynamically and replace them with the values
-    processed = Template(dockerfile).substitute(
+    convert_to_list_args = lambda x: x.split(" ") if x else []
+
+    return Template(dockerfile).substitute(
         VERSION=version,
-        INSTALL_COMMAND=install_command
+        INSTALL_COMMAND=install_command,
+        RUN_COMMAND=convert_to_list_args(run_command),
     )
 
 
+def dockerize(
+    language: LANGUAGES,
+    framework: FRAMEWORKS,
+    version: str,
+    install_command: Optional[str] = None,
+    run_command: Optional[str] = None,
+):
+    assert language in LANGUAGES, f"Language {language} not supported"
+    assert (
+        version in VERSIONS[language]
+    ), f"Version {version} not supported for {language}"
+    if install_command is None:
+        install_command = INSTALL_COMMANDS[language]
+    if run_command is None:
+        run_command = OPTIONS[framework][COMMAND_TYPES.RUN_CMD]
 
+    with open(f"./templates/{language}/{framework}", "r") as raw_dockerfile:
+        dockerfile = raw_dockerfile.read()
+        dockerfile = process(
+            dockerfile, language, framework, version, install_command, run_command
+        )
 
-
-
+    # Dockerize the language and version
+    pass
 
 
 def test_dockerize():
@@ -43,3 +58,6 @@ def test_dockerize():
     CMD ["python", "app.py"]
     """
     assert command == DOCKERFILE
+
+
+
