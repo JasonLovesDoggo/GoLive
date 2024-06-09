c754340f31e1f41e816d8403ba65e715b0e6de59
Jason Cameron <git@jasoncameron.dev>
2024-06-09 10:56:50 +0000

    switch to full deploy blocks
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 docker run --privileged --rm tonistiigi/binfmt --install all
 docker buildx create --use --name amd64-builder
 /bin/cp -f ${BUILD_PATH}/Dockerfile .
-docker build -t image . --platform linux/amd64 --load
+docker buildx build --platform=linux/amd64 -t image . --load
 cd ${BUILD_PATH}
 docker image save image > image.tar
 scp -oStrictHostKeyChecking=no -i ./id_ed25519 image.tar json@${HOST}:/home/json/image.tar
@@ -16,5 +16,5 @@ ssh  -oStrictHostKeyChecking=no  json@${HOST} -i ./id_ed25519 << "EOF"
   sudo docker ps -q --filter "publish=${PORT}" | xargs sudo docker rm -f
   sudo docker run -d -p ${PORT}:${PORT} $$(sudo docker images | awk '{print $$1}' | awk 'NR==2')
 
-  sudo caddy reverse-proxy --from :80 --to localhost:${PORT} &
+  ${CADDY_DEPLOY_BLOCK}
 EOF
