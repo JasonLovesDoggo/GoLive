f4a8678025ead681ac39997883b67d8744e3d309
Jason Cameron <git@jasoncameron.dev>
2024-06-09 10:04:06 +0000

    make it ignored Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- src/autodeploy/create.py
+++ src/autodeploy/create.py
@@ -5,19 +5,25 @@ from ..containerizer.types import Options
 from ..containerizer.constants import LANGUAGES
 from ..containerizer.defaults import FRAMEWORKS
 from pathlib import Path
-
+import json
 def create(options: Options):
     template_path = os.path.join(os.path.dirname(__file__), "main.tftemplate")
     with open(template_path, "r") as f:
         template = f.read()
 
     template_path = os.path.join(Path(__file__).parent.parent.parent, "build")
+    with open(os.path.join(Path(__file__).parent.parent.parent, "d"), "r") as raw_json:
+        raw_json = raw_json.read()
+        json_data = json.loads(raw_json)
+        PROJECT_ID = json_data["project_id"]
+
+
 
     with open(f"{template_path}/main.tf", "w") as main:
         template = string.Template(template).substitute(
             NAME=urllib.parse.quote(str(options.app_dir).split("/")[-1]).casefold(),
             PORT=options.port,
-            PROJECT_ID="new-proj-425823",
+            PROJECT_ID=PROJECT_ID,
             SSH_PUB_KEY=options.ssh_pub_key,
         )
         main.write(template)
